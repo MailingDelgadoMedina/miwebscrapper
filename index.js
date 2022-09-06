f81e6d950f908bb75aea7e73e6df63859d9507bd
MailingDelgadoMedina <mailing.delgado.scitexas@gmail.com>
2022-09-06 00:44:45 -0400

    finished the promise to scrape all the titles in the page

--- index.js
+++ index.js
@@ -5,6 +5,22 @@ const PORT = 8000;
 
 
 const app = express();
+const url = 'https://www.theguardian.com/uk';
+
+axios.get(url)
+    .then(response => {const html = response.data
+  const $ = cheerio.load(html)
+    const articles = []
+
+  $('.fc-item__title', html).each(function() {
+const title = $(this).text()
+const url = $(this).find('a').attr('href')
+articles.push({title, url})
+
+})
+console.log(articles)
+}).catch(console.error)
+
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
