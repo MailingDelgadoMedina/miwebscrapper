538930187d86b7ed44f71873a18229392d0718ac
MailingDelgadoMedina <mailing.delgado.scitexas@gmail.com>
2022-09-06 00:25:37 -0400

    imported axios, cheerio, express and set up the port on index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+const express = require('express');
+const PORT = 8000;
+
+
+const app = express();
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
